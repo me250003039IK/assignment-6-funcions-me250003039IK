aff5b09a1db019d9fe8d08e7370c87cfe7691258
Ishan Khekare <me250003039@iiti.ac.in>
2025-09-04 13:22:43 +0530

    Update product_except_index.cpp

--- product_except_index.cpp
+++ product_except_index.cpp
@@ -2,24 +2,36 @@
 #include <vector>
 using namespace std;
 
-vector<long long> productExceptIndex(const vector<int>& arr) {
-    int size = arr.size();
-    vector<long long> output(size, 1);
-
+int productExceptIndex(const vector<int>& arr, int index) {
+    int product = 1;
     // TODO: complete the function as per instructions
-    
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (i != index)
+        {
+        product *= arr[i];
+        }
+        // cout << arr[i] << endl;
+    }
+    return product;
 }
 
 int main() {
     int n; cin >> n;
     vector<int> arr(n);
-    for (int i=0; i<n; i++) cin >> arr[i];
-
-    vector<long long> res = productExceptIndex(arr);
-    for(auto v : res)
-        cout << v << " ";
-    cout << "\n";
-
+    for (int i=0; i<n; i++) 
+    {
+        cin >> arr[i];
+    }
+    vector<long long> res(n);
+    for (int i = 0; i < res.size(); i++)
+    {
+        res[i] = productExceptIndex(arr, i);
+    }
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
-
